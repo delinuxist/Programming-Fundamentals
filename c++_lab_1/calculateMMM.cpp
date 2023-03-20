2c989ec1b663e09e0052f245f4ccce0361198a02
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-03-20 21:42:42 +0000

    mean,mode and median completed

--- c++_lab_1/calculateMMM.cpp
+++ c++_lab_1/calculateMMM.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// sort array function
 int * sorted(int array[5]) {
 	for (int i=0; i<5;i++){
 		for(int j=i+1; j<5;j++){
@@ -8,21 +9,19 @@ int * sorted(int array[5]) {
 				int temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
-
 			}
 		}	
 	}
 	return array;
 }
 
-void mode(int array[5]) {
+int mode(int array[5]) {
 	int numCount[5];
+	int *tempCount;
 	int counter = 0;
-	cout<<"Sorted Array"<<endl;
-	for(int i=0;i<5;i++){
-		cout<<array[i]<<endl;
-}
-cout<<"Inside Mode"<<endl;
+	int mode;
+
+// using for loop to count number occurences and store in numCount array
 for(int i=0;i<5;i++){
 	counter = 0;
 	for(int j=0;j<5;j++){
@@ -33,12 +32,28 @@ for(int i=0;i<5;i++){
 	}
 } 
 
+// assigning numCount to tempCount
+tempCount = numCount;
+
+// loop through the tempCount to find the highest occurence number
 for(int i=0;i<5;i++){
-	
+	if(tempCount[0] < tempCount[i]){
+		tempCount[0] = tempCount[i];
+	}
 }
 
+// loop through numCount for most occurence index and insert index in array
+for(int i=0;i<5;i++){
+	if(numCount[i] == tempCount[0]){
+		mode = array[i];
+		break;
+	}
 }
 
+return mode;
+}
+
+// median function
 int median(int array[5]) {
 	int value;
 	for(int i=0;i<5;i++) {
@@ -54,29 +69,29 @@ int main() {
 	int *sortedArray;
 	int sum = 0;
 
-	cout<<"Enter num1"<<endl;
-	cin>>numsArray[0];
-	cout<<"Enter num2"<<endl;
-	cin>>numsArray[1];
-	cout<<"Enter num3"<<endl;
-	cin>>numsArray[2];
-	cout<<"Enter num4"<<endl;
-	cin>>numsArray[3];
-	cout<<"Enter num5"<<endl;
-	cin>>numsArray[4];
+// using for loop to get user input
+for(int i=0;i<5;i++){
+	cout<<"Enter num"<<i<<endl;
+	cin>>numsArray[i];
+}
 
-	for(int i =0;i<5;i++){
-		sum+=numsArray[i];
-	}
+// total sum of numbers in the array
+for(int i =0;i<5;i++){
+	sum+=numsArray[i];
+}
 
-	double mean = sum/5;
+	double mean = sum/5.0;
 
+	//storing the sorted array in sortedArray variable
 	sortedArray = sorted(numsArray);
-	mode(sortedArray);
+
+	int modeValue = mode(sortedArray);
+
 	int medianValue = median(sortedArray);
 
+
 	cout<<"Mean "<<mean<<endl;
-	//cout<<"Mode "<<mode<<endl;
+	cout<<"Mode "<<modeValue<<endl;
 	cout<<"Median "<<medianValue<<endl;
 
 	return 0;
