1f91283bbd46d547bc0aebd944ce5654bb827c85
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-03-23 12:10:04 +0000

    refactored code using pointers and reference

--- c++_lab_1/calculateMMM.cpp
+++ c++_lab_1/calculateMMM.cpp
@@ -2,95 +2,117 @@
 using namespace std;
 
 // sort array function
-int * sorted(int array[5]) {
-	for (size_t i=0; i<5;i++){
-		for(size_t j=i+1; j<5;j++){
-			if(array[i] > array[j]){
+void sorted(int array[], int *length)
+{
+	for (size_t i = 0; i < *(length); i++)
+	{
+		for (size_t j = i + 1; j < *(length); j++)
+		{
+			if (array[i] > array[j])
+			{
 				int temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
 			}
-		}	
+		}
 	}
-	return array;
 }
 
-int mode(int array[5]) {
-	int numCount[5];
+int mode(int array[], int &length)
+{
+	int numCount[length];
 	int mode;
 
-// using for loop to count number occurrences and store in numCount array
-for(size_t i=0;i<5;i++){
-	int counter = 0;
-	for(size_t j=0;j<5;j++){
-		if(array[i] == array[j]){
-			counter++;
+	// using for loop to count number occurrences and store in numCount array
+	for (size_t i = 0; i < length; i++)
+	{
+		int counter = 0;
+		for (size_t j = 0; j < length; j++)
+		{
+			if (array[i] == array[j])
+			{
+				counter++;
+			}
+			numCount[i] = counter;
 		}
-		numCount[i] = counter;
 	}
-} 
 
-// assigning maxOccurrence to the first value in numCount array
-int maxOccurrence = numCount[0];
+	// assigning maxOccurrence to the first value in numCount array
+	int maxOccurrence = numCount[0];
 
-// loop through the numCount to find the highest occurrence number
-for(size_t i=0;i<5;i++){
-	if(maxOccurrence < numCount[i]){
-		maxOccurrence = numCount[i];
+	// loop through the numCount to find the highest occurrence number
+	for (size_t i = 0; i < length; i++)
+	{
+		if (maxOccurrence < numCount[i])
+		{
+			maxOccurrence = numCount[i];
+		}
 	}
-}
 
-// loop through numCount for most occurrence index and insert index in array
-for(size_t i=0;i<5;i++){
-	if(numCount[i] == maxOccurrence){
-		mode = array[i];
-		break;
+	// loop through numCount for most occurrence index and insert index in array
+	for (size_t i = 0; i < length; i++)
+	{
+		if (numCount[i] == maxOccurrence)
+		{
+			mode = array[i];
+			break;
+		}
 	}
-}
 
-return mode;
+	return mode;
 }
 
 // median function
-int median(int array[5]) {
+int median(int array[], int &length)
+{
 	int value;
-	for(size_t i=0;i<5;i++) {
-		if(i == 2){
-		 value=array[i];
+	for (size_t i = 0; i < length; i++)
+	{
+		if (i == 2)
+		{
+			value = array[i];
 		}
 	}
 	return value;
 }
 
-int main() {
+int main()
+{
 	int numsArray[5];
-	int *sortedArray;
 	int sum = 0;
 
-// using for loop to get user input
-for(size_t i=0;i<5;i++){
-	cout<<"Enter num"<<i+1<<endl;
-	cin>>numsArray[i];
-}
+	int length = sizeof(numsArray) / sizeof(numsArray[0]);
 
-// total sum of numbers in the array
-for(int i =0;i<5;i++){
-	sum+=numsArray[i];
-}
+	// using for loop to get user input
+	for (size_t i = 0; i < length; i++)
+	{
+		cout << "Enter num" << i + 1 << endl;
+		cin >> numsArray[i];
+	}
 
-	double mean = sum/5.0;
+	// total sum of numbers in the array
+	for (int i = 0; i < length; i++)
+	{
+		sum += numsArray[i];
+	}
 
-	//storing the sorted array in sortedArray variable
-	sortedArray = sorted(numsArray);
+	double mean = sum / 5.0;
 
-	int modeValue = mode(sortedArray);
+	// sorting the array;
+	sorted(numsArray, &length);
 
-	int medianValue = median(sortedArray);
+	for (int a : numsArray)
+	{
+		cout << a << endl;
+	}
 
+	int modeValue = mode(numsArray, length);
 
-	cout<<"Mean "<<mean<<endl;
-	cout<<"Mode "<<modeValue<<endl;
-	cout<<"Median "<<medianValue<<endl;
+	int medianValue = median(numsArray, length);
+
+	cout << "Mean " << mean << endl;
+	cout << "Mode " << modeValue << endl;
+	cout << "Median " << medianValue << endl;
 
 	return 0;
-}	
+}
