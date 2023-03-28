6132ad1f6255b3c6ac427392e977fa08863fe014
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-03-28 20:57:52 +0000

    changes made

--- c++_lab_4/question2.cpp
+++ c++_lab_4/question2.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 namespace my
 {
   int strcmp(const char *l, const char *r)
   {
+    return 0;
   }
 
   int strlen(const char *s)
   {
+    return 0;
   }
 }
 
+struct CarData
+{
+  char req[8];
+  int year;
+};
+
 int main()
 {
+  CarData mine, yours;
+  mine.year = 20;
+  yours.year = 10;
+
+  cout << mine.year << endl;
+  cout << yours.year << endl;
 }
