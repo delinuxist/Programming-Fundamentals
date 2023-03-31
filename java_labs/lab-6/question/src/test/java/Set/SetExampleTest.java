362f5e531a7f1b245cd014971baee16a914b2611
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-03-31 17:52:21 +0000

    lab-6 not complete

--- java_labs/lab-6/question/src/test/java/Set/SetExampleTest.java
+++ java_labs/lab-6/question/src/test/java/Set/SetExampleTest.java
@@ -1,4 +1,20 @@
+package Set;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SetExampleTest {
-  
+
+    @BeforeEach
+    void setUp(){
+        SetExample underTest = new SetExample();
+    }
+
+    @Test
+    void shouldTest(){
+        assertTrue(true);
+    }
+
 }
