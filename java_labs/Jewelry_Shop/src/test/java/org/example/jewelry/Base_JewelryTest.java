a387d913b210f5f7dc7f4f484a107c3fb16b1226
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-04-03 06:08:42 +0000

    test and some few changes to files

---
+++ java_labs/Jewelry_Shop/src/test/java/org/example/jewelry/Base_JewelryTest.java
@@ -0,0 +1,21 @@
+package org.example.jewelry;
+
+import org.junit.jupiter.api.Test;
+
+
+class Base_JewelryTest {
+
+
+    @Test
+    void compareObjects() {
+        //given
+
+        
+        //when
+
+
+        //then
+    }
+
+
+}
